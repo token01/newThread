f765347495164add72cb981ff0f822faa392158c
sunzy <token01@126.com>
2018-03-27 18:02:03 +0800

    add something for no visibility

--- src/main/java/com.token01/thread/NoVisibility.java
+++ src/main/java/com.token01/thread/NoVisibility.java
@@ -2,21 +2,22 @@ package com.token01.thread;
 
 /**
  * @author abel-sun 2016/4/4.
- *
+ * <p>
  * 静态类成员变量虽然可以在不同的线程之间共享，
  * 但是，根据Java规范定义的内存模型，
  * 在本例中，只有当main线程的写操作(ready=true)在ReaderThread的读操作(while(!ready))之前执行，
- *
  */
 public class NoVisibility {
-    private static  boolean ready;
+    private static boolean ready;
     private static int number;
 
-    private static class ReaderThread extends  Thread{
+    private static class ReaderThread extends Thread {
+        @Override
         public void run() {
-            while (!ready)
+            while (!ready) {
                 Thread.yield();
-            System.out.println(number);
+                System.out.println(number);
+            }
         }
     }
 
